48869ffd4a16c71ac571d4d6422c25106f6eae9f
Ivan Tsuman <itsuman@e2r12p8.unit.ua>
2017-11-09 16:14:29 +0200

    add ex02 in d7

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -20,12 +20,39 @@ template <typename T>
 class Array
 {
 	private:
-
+		unsigned int 	length;
+		T 				*array;
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array const &src);
-		~Array();
-		Array &operator=(Array const &src);
-		
+		Array<T>() : array(NULL), length(0) {};
+		Array<T>(unsigned int n) : length(n), array(new T[n]) {};
+		Array<T>(Array<T> const &src) {*this = src;};
+
+		~Array<T>();
+
+////// operator=
+		Array<T> &operator=(Array<T> const &src){
+			if (this != &src)
+			{
+				try{
+					for(unsigned int i = 0; i < length; i++){
+						array[i] = src.array[i];
+					}
+				}
+				catch (std::exception ex){
+					std::cout << "Too long array." << std::endl;
+				}
+			}
+			return *this;
+		};
+
+/////// operator[]
+		T 		operator[](int n){
+			if(n >= length)
+				throw std::exception();
+			return array[n];
+		};
+
+		unsigned int	size() {return length;};
 };
+
+#endif
